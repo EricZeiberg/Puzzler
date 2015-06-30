b519d57ea83407b155c891a138b6edcf527d8b98
Eric Zeiberg <ejzeiberg@Gmail.com>
2015-06-30 10:01:30 -0700

    Add right click detection using RayCastHit

--- Assets/Scripts/RightClick.cs
+++ Assets/Scripts/RightClick.cs
@@ -5,13 +5,24 @@ public class RightClick : MonoBehaviour {
 
 	Ray ray;
 	RaycastHit hit;
+	public GameObject parent_to;
 	
 	void Update()
 	{
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out hit))
-		{
-			print (hit.collider.name);
+		if (Input.GetMouseButton (0)) {
+			ray = Camera.allCameras[0].ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit))
+			{
+				if (hit.collider.gameObject.tag.Equals("Holdable")) {
+					hit.collider.transform.parent.SetParent(parent_to.transform);
+				}
+			}
 		}
+	}
+
+	void OnMouseDown() {
+
+
+
 	}
 }
