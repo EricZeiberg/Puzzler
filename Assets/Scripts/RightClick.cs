4c174d9b8ab25566432aa7fb72bbeaaa3efd40a0
Eric Zeiberg <ejzeiberg@Gmail.com>
2015-06-30 10:53:46 -0700

    Work on handheld objects

--- Assets/Scripts/RightClick.cs
+++ Assets/Scripts/RightClick.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class RightClick : MonoBehaviour {
 
 	Ray ray;
 	RaycastHit hit;
 	public GameObject parent_to;
+	public GameObject hand_obj;
 	
 	void Update()
 	{
@@ -14,15 +16,14 @@ public class RightClick : MonoBehaviour {
 			if(Physics.Raycast(ray, out hit))
 			{
 				if (hit.collider.gameObject.tag.Equals("Holdable")) {
-					hit.collider.transform.parent.SetParent(parent_to.transform);
+					Vector3 colliderVector = hit.collider.gameObject.transform.position;
+					Quaternion rotation = hit.collider.gameObject.transform.rotation;
+					hit.collider.gameObject.transform.SetParent(parent_to.transform);
+					hit.collider.gameObject.transform.localPosition = new Vector3(11, -15, 36);
+					hit.collider.gameObject.transform.rotation.Set(11, 8, -1, rotation.w);
+					hand_obj.SetActive(false);
 				}
 			}
 		}
-	}
-
-	void OnMouseDown() {
-
-
-
 	}
 }
