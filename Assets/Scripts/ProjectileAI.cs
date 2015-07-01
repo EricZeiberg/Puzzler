1e375f95591fb958ff8c1129bd9f6f7179ad6000
Eric Zeiberg <ejzeiberg@Gmail.com>
2015-07-01 16:14:42 -0700

    Try to fix sentry

---
+++ Assets/Scripts/ProjectileAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileAI : MonoBehaviour {
+
+	public float speed = 500f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+		print ("Sent!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/SentryAI.cs
+++ Assets/Scripts/SentryAI.cs
@@ -7,10 +7,11 @@ public class SentryAI : MonoBehaviour {
 	public float delay;
 	public int projectileAmount = 1;
 	public int minDistance = 100;
-	public int speed = 50;
 
 	public GameObject target;
 	public GameObject original_Proj;
+
+	float internalDelay;
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +21,9 @@ public class SentryAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Vector3.Distance (target.transform.position, transform.position) <= minDistance) {
-			StartCoroutine(Shoot());
+			Object proj = Instantiate(original_Proj, original_Proj.transform.position, original_Proj.transform.rotation);
 		}
 	}
 
-	IEnumerator Shoot()
-	{
-		for (float timer = delay; timer >= 0; timer -= Time.deltaTime)
-			yield return 0;
 
-		GameObject proj = Instantiate(original_Proj, original_Proj.transform.position, original_Proj.transform.rotation) as GameObject;
-		proj.GetComponent<Rigidbody>().AddForce(Vector3.up + Vector3.forward * speed);
-
-	}
 }
